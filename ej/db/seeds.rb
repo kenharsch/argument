b512f01529c38858cc8b3b41f9a0521ec548d0a8
kenharsch <kharsch@brandeis.edu>
2016-01-25 14:47:23 -0500

    added new seeds

--- ej/db/seeds.rb
+++ ej/db/seeds.rb
@@ -8,4 +8,17 @@
     User.create(name: 'Michelle', email: 'msisti@brandeis.edu', password: '12345678', admin: 'true')
     User.create(name: 'Nishan', email: 'acharyan@brandeis.edu', password: '12345678', admin: 'true') 
     User.create(name: 'Ken', email: 'kharsch@brandeis.edu', password: '12345678', admin: 'true') 
+
+    User.create(name: 'JoeC', email: 'jcarter@brandeis.edu', password: '12345678') 
+    User.create(name: 'Kruk', email: 'jkruk@brandeis.edu', password: '12345678') 
+    User.create(name: 'Vida', email: 'vblue@brandeis.edu', password: '12345678') 
+    User.create(name: 'Babe', email: 'gruth@brandeis.edu', password: '12345678') 
+    User.create(name: 'Grover', email: 'mhargrove@brandeis.edu', password: '12345678') 
+    User.create(name: 'Say Hey', email: 'wmayes@brandeis.edu', password: '12345678') 
    
+   Assignment.create(title: 'Assignment 1', description: 'Assignment 1 description', draft_deadline: '2016-01-30 05:00:00', final_deadline: '2016-01-31 05:00:00')
+   Assignment.create(title: 'Assignment 2', description: 'Assignment 2 description', draft_deadline: '2016-02-15 05:00:00', final_deadline: '2016-02-17 05:00:00')
+
+   Position.create(title: 'For', assignment_id: 1)
+   Position.create(title: 'Against', assignment_id: 1)
+   Position.create(title: 'Neutral', assignment_id: 1)
